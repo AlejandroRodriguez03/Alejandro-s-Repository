789621c85fce02f46b8436adda3146d2216c2684
Alejandro <ale2003ven@gmail.com>
2022-09-19 07:53:25 -0400

    EJERCICIOS

---
+++ EJERCICIOS/EJERCICIO 3.py
@@ -0,0 +1,20 @@
+DI = 3480
+
+print("El saldo inicial es de:{}")
+
+R1 = 96
+
+Saldo_actual_a_la_fecha_04_11 = DI - R1
+
+D1=1200
+
+Saldo_actual_a_la_fecha_04_17 = Saldo_actual_a_la_fecha_04_11 + D1
+
+intereses= Saldo_actual_a_la_fecha_04_17 * 0.03
+
+Saldo_actual_a_la_fecha_04_30 = Saldo_actual_a_la_fecha_04_17 - intereses
+
+R2=51
+
+Saldo_actual_a_la_fecha_05_02= Saldo_actual_a_la_fecha_04_30 - R2
+
---
+++ EJERCICIOS/EJERCICIO 4.py
@@ -0,0 +1,13 @@
+N1 = (input("inserte numero 1:"))
+N2 = (input("inserte numero 2:"))
+N3 = N1/N2
+print (N3)
+
+if N1.isnumeric():
+   N1= float()
+
+if N3 == 0:
+    print("error")
+
+else:
+    print( N3 )
